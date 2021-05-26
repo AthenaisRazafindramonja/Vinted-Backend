57ea7f89723f49eba34e072e50f381245b7fb0e2
Athenaïs Razafindramonja <athenais.raza@gmail.com>
2021-05-26 10:56:44 +0200

    cors

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 const express = require("express");
 const formidable = require("express-formidable");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 app.use(formidable());
+app.use(cors());
 
 // Import des routes
 const userRoutes = require("./routes/user");
